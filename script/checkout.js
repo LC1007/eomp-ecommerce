40d69c879333bafba39b9537aa60efeb8760f5cd
LC1007 <tonyg7212@gmail.com>
2023-06-13 16:38:53 +0200

    checkoutPage almost complete

---
+++ script/checkout.js
@@ -0,0 +1,26 @@
+let checkoutArr = 
+JSON.parse(localStorage.getItem('checkoutData')) ? 
+JSON.parse(localStorage.getItem('checkoutData')) : []
+let summaryCards = document.querySelector('#summaryCards')
+
+console.log(checkoutArr);
+
+summaryCards.innerHTML = ''
+checkoutArr.forEach((droneCheckout) =>{
+    summaryCards.innerHTML +=
+    `
+    <div class="card mb-3 p-0" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${droneCheckout.img}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${droneCheckout.title}</h5>
+              <p class="card-text">${droneCheckout.description}</p>
+            </div>
+          </div>
+        </div>
+    </div>
+    `
+})
